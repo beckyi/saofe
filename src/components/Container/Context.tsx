6d98704366485ebac9992d476af619c1972ca13d
최재은 <beckyi@douzone.com>
2021-03-30 23:53:43 +0900

    [master] add globals variable

--- src/components/Container/Context.tsx
+++ src/components/Container/Context.tsx
@@ -1,11 +1,42 @@
 import React, {createContext, useState} from "react";
+import BrowserStorage from "../../utils/BrowserStorage";
+import NAME from "../../utils/Enum";
 
 interface IProps {
   children?: React.ReactNode;
 };
-interface IState {};
 
-const BackContext = createContext({
+interface IUserInfo {
+  [NAME.USERNAME]: string;
+  [NAME.BIRTH]: string;
+  [NAME.COMMENT]: string;
+}
+
+interface IState {
+  userInfo: IUserInfo | object;
+  keywords: Array<string>;
+};
+
+interface Props {
+  setUserInfo: (userInfo:object) => void;
+  setKeywords: (keywords:any) => void;
+}
+
+interface IconText {
+  state : IState;
+  actions: Props;
+}
+
+const storage = new BrowserStorage("local");
+const storageUser = `${NAME.GROUP}USER`;
+const storageBackground = `${NAME.GROUP}BACKGROUND`;
+const userForm = {
+  [NAME.USERNAME]: "",
+  [NAME.BIRTH]: "",
+  [NAME.COMMENT]: "",
+};
+
+const BackContext = createContext<IconText>({
   state: {
     userInfo: {},
     keywords: []
@@ -13,19 +44,26 @@ const BackContext = createContext({
   actions: {
     setUserInfo: () => {},
     setKeywords: () => {}
-  });
+  }
+});
 
 const {Provider, Consumer: BackConsumer} = BackContext;
 
-const BackProvider = ({children}) => {
-  const [userInfo, setUserInfo] = useState({});
-  const [keywords, setKeywords] = useState([]);
+const BackProvider:React.FunctionComponent<IProps> = (props:IProps) => {
+  const {children} = props;
+  const [userInfo, setUserInfo] = useState(storage.getItem(storageUser) || userForm);
+  const [keywords, setKeywords] = useState(storage.getItem(storageBackground) || []);
 
-    return(
-      <Provider value={{state:{userInfo,keywords}, actions: {setUserInfo,setKeywords}}}>
-        {children}
-      </Provider>
-    );
+  const val: {state:IState, actions: Props} = {
+    state:{userInfo, keywords}, 
+    actions: {setUserInfo, setKeywords}
+  }
+
+  return(
+    <Provider value={val}>
+      {children}
+    </Provider>
+  );
 }
 
 export default {BackProvider, BackConsumer};
